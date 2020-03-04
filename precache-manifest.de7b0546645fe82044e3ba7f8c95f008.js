65378772b9da9791ac2a6577faf7ca0df178bdd9
Sofiqul Islam <schoolwab@gmail.com>
2020-03-04 16:44:17 +0600

    Updates

--- precache-manifest.de7b0546645fe82044e3ba7f8c95f008.js
+++ precache-manifest.de7b0546645fe82044e3ba7f8c95f008.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0e20e4fac3abed2fc1c9cf9cb591a435",
+    "revision": "379801da58b98a2152b87eaefbc4a583",
     "url": "/pabnainfo/index.html"
   },
   {
-    "revision": "e23bec37fa39795b95b3",
+    "revision": "65750c40b1e98e8f7730",
     "url": "/pabnainfo/static/css/2.31677d80.chunk.css"
   },
   {
-    "revision": "afd1041190a56b22aa55",
+    "revision": "977fa2a9494f844c7b53",
     "url": "/pabnainfo/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "e23bec37fa39795b95b3",
-    "url": "/pabnainfo/static/js/2.5bc3e338.chunk.js"
+    "revision": "65750c40b1e98e8f7730",
+    "url": "/pabnainfo/static/js/2.fb75b328.chunk.js"
   },
   {
     "revision": "68fab0655c55ec347ec9a935d49eefb8",
-    "url": "/pabnainfo/static/js/2.5bc3e338.chunk.js.LICENSE.txt"
+    "url": "/pabnainfo/static/js/2.fb75b328.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "afd1041190a56b22aa55",
-    "url": "/pabnainfo/static/js/main.9135700d.chunk.js"
+    "revision": "977fa2a9494f844c7b53",
+    "url": "/pabnainfo/static/js/main.3ddf67cd.chunk.js"
   },
   {
     "revision": "84d277fe00d785d9aa4d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pabnainfo/precache-manifest.0f0964af480a7c6f44a2b1fa2df7ee70.js"
+  "/pabnainfo/precache-manifest.de7b0546645fe82044e3ba7f8c95f008.js"
 );
 
 self.addEventListener('message', (event) => {
